61ff7b4966c1c319fda232b7c59b5ab2d293c8ab
Shubham Parihar <shubhamparihar391@gmail.com>
2022-08-05 03:48:21 +0530

    feat: add DirectedGraph and dfs

---
+++ src/algorithms/dfs.rs
@@ -0,0 +1,50 @@
+use crate::structures::directed_graph::DirectedGraph;
+
+pub fn dfs(graph: DirectedGraph, start_node: i32) -> Vec<i32> {
+    if !graph.contains_node(start_node) {
+        panic!("'start_node' not found in the provided graph");
+    }
+    let mut stack = vec![];
+    let mut visited = vec![];
+    stack.push(start_node);
+    while !stack.is_empty() {
+        let current_node = stack.pop().unwrap();
+        let mut neighbors = graph.get_neighbors(current_node);
+        stack.append(&mut neighbors);
+        visited.push(current_node);
+    }
+    visited
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::structures::directed_graph::DirectedGraph;
+
+    use super::dfs;
+
+    #[test]
+    fn it_performs_dfs() {
+        let mut graph = DirectedGraph::new();
+        graph
+            .add_edge(1, 2)
+            .add_edge(1, 3)
+            .add_edge(2, 4)
+            .add_edge(2, 5);
+
+        let result = dfs(graph, 1);
+        assert_eq!(result, vec![1, 3, 2, 5, 4]);
+    }
+
+    #[test]
+    #[should_panic(expected = "'start_node' not found in the provided graph")]
+    fn it_performs_dfs_with_invalid_start_node() {
+        let mut graph = DirectedGraph::new();
+        graph
+            .add_edge(1, 2)
+            .add_edge(1, 3)
+            .add_edge(2, 4)
+            .add_edge(2, 5);
+
+        dfs(graph, 6);
+    }
+}
---
+++ src/structures/directed_graph.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+
+pub struct DirectedGraph {
+    _map: HashMap<i32, Vec<i32>>,
+}
+
+impl DirectedGraph {
+    pub fn new() -> DirectedGraph {
+        return DirectedGraph {
+            _map: HashMap::new(),
+        };
+    }
+}
+
+impl DirectedGraph {
+    pub fn add_edge(&mut self, src_node: i32, des_node: i32) -> &mut DirectedGraph {
+        if let Some(v) = self._map.get_mut(&src_node) {
+            v.push(des_node);
+        } else {
+            self._map.insert(src_node, vec![des_node]);
+        }
+
+        if let None = self._map.get_mut(&des_node) {
+            self._map.insert(des_node, vec![]);
+        }
+        self
+    }
+
+    pub fn get_neighbors(&self, src_node: i32) -> Vec<i32> {
+        match self._map.get(&src_node) {
+            Some(v) => v.to_vec(),
+            None => {
+                vec![]
+            }
+        }
+    }
+
+    pub fn contains_node(&self, node: i32) -> bool {
+        return self._map.contains_key(&node);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DirectedGraph;
+    use std::collections::HashMap;
+
+    #[test]
+    fn it_adds_edges() {
+        let mut graph = DirectedGraph::new();
+        graph
+            .add_edge(1, 2)
+            .add_edge(1, 3)
+            .add_edge(2, 4)
+            .add_edge(2, 5);
+
+        let mut map = HashMap::new();
+        map.insert(1, vec![2, 3]);
+        map.insert(2, vec![4, 5]);
+        map.insert(3, vec![]);
+        map.insert(4, vec![]);
+        map.insert(5, vec![]);
+
+        assert_eq!(graph._map, map);
+    }
+
+    #[test]
+    fn it_gets_neighbors() {
+        let mut graph = DirectedGraph::new();
+        graph
+            .add_edge(1, 2)
+            .add_edge(1, 3)
+            .add_edge(2, 4)
+            .add_edge(2, 5);
+
+        assert_eq!(graph.get_neighbors(1), vec![2, 3]);
+        assert_eq!(graph.get_neighbors(2), vec![4, 5]);
+        assert_eq!(graph.get_neighbors(3), vec![]);
+    }
+}
